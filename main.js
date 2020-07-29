196625df2dd6a4effc9097713b59b6b41caa2c2e
Brayden Royston <brayden.royston10@gmail.com>
2020-07-28 21:50:39 -0400

    Added images, updated index

---
+++ main.js
@@ -0,0 +1,43 @@
+$(window).load(function() {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+});
+
+var typed = new Typed('.type', {
+        strings: [
+            'Double Degree.', 
+            'Learner.',
+            'Speaker.',
+            'DevOps Consultant.',
+            'Developer.',
+            'Business Analyst.',
+            'Engineering Consultant.',
+            'Waterloo Warrior.',
+            'Laurier Goldenhawk.',
+            'SHAD Fellow.',
+            'Westlane Spartan.'
+        ],
+        typeSpeed: 60,
+        backSpeed: 60,
+        loop: true
+});
+
+
+
+const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper')
+
+const portoflioImgs = document.querySelectorAll('.logoWrapper')
+
+portfolioItems.forEach(portfolioItem => {
+    portfolioItem.addEventListener('mouseover', () => {
+        portfolioItem.childNodes[1].classList.add('img-darken');
+        portfolioItem.childNodes[2].childNodes[1].classList.add('img-fade-in');
+
+    })
+
+    portfolioItem.addEventListener('mouseout', () => {
+        portfolioItem.childNodes[1].classList.remove('img-darken');
+        portfolioItem.childNodes[2].childNodes[1].classList.remove('img-fade-in');
+
+    })
+})
